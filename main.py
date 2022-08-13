24db81b040139f98836e25f7b14c171dd144614f
Fernanda Oliveira <81169517+fernandaop@users.noreply.github.com>
2022-08-13 12:07:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import urllib.request
-import json
 import PySimpleGUI as gui
 import win32com.client as win32
-import pandas as pd
-import sys
-import csv
 import json
 import sqlalchemy as sql
 import datetime
@@ -12,29 +8,17 @@ import datetime
 ssl_args = {'ssl_ca': "DigiCertGlobalRootCA.crt.pem"}
 engine =sql.create_engine('mysql+pymysql://fernanda.pereira:2uwHbaCukUl&yWYUXxkC@vdwh.mysql.database.azure.com:3306/dwh', connect_args=ssl_args)
 conn = engine.connect()
-response = engine.execute('select cra_sch.ticker_symbol, cra_inst.corporation_name, cra_sch.event_name, cra_sch.event_unit_price, cra_sch.payment_date from dwh.vw_up2data_fixed_income_cra_schedule as cra_sch join dwh.vw_up2data_fixed_income_cra_instrument as cra_inst on (cra_sch.ticker_symbol = cra_inst.ticker_symbol)')
-def cra(response):
-    cra = []
-    dic = {}
+response = engine.execute('select cra_sch.ticker_symbol, cra_inst.corporation_name, cra_sch.event_name, cra_sch.event_unit_price, cra_sch.payment_date from dwh.vw_up2data_fixed_income_cra_schedule as cra_sch join dwh.vw_up2data_fixed_income_cra_instrument as cra_inst on (cra_sch.ticker_symbol = cra_inst.ticker_symbol) where (cra_inst.corporation_name = "VIRGO COMPANHIA DE SECURITIZACAO") or (cra_inst.corporation_name = "VIRGO II COMPANHIA DE SECURITIZACAO") or (cra_inst.corporation_name = "ISEC SECURITIZADORA S.A"); ')
+response1 = engine.execute('select cri_sch.ticker_symbol, cri_inst.corporation_name, cri_sch.event_name, cri_sch.event_unit_price, cri_sch.payment_date from dwh.vw_up2data_fixed_income_cri_schedule as cri_sch join dwh.vw_up2data_fixed_income_cri_instrument as cri_inst on (cri_sch.ticker_symbol = cri_inst.ticker_symbol) where (cri_inst.corporation_name = "VIRGO COMPANHIA DE SECURITIZACAO") or (cri_inst.corporation_name = "VIRGO II COMPANHIA DE SECURITIZACAO") or (cri_inst.corporation_name = "ISEC SECURITIZADORA S.A");')
+def crai(response):
+    crai= []
     for row in response:
-        if row[1] == 'ISEC SECURITIZADORA S.A' or row[1] == 'VIRGO II COMPANHIA DE SECURITIZACAO' or row[1] == 'VIRGO COMPANHIA DE SECURITIZACAO':
-            if row[4] == datetime.date.today():
-                dic["id"] = row[0]
-                dic["tipo"] = row[2]
-                dic["preço"] = round(row[3],3)
-                cra.append(dic)
-    return cra
-cra = cra(response)
-response1 = engine.execute('select cri_sch.ticker_symbol, cri_inst.corporation_name, cri_sch.event_name, cri_sch.event_unit_price, cri_sch.payment_date from dwh.vw_up2data_fixed_income_cri_schedule as cri_sch join dwh.vw_up2data_fixed_income_cri_instrument as cri_inst on (cri_sch.ticker_symbol = cri_inst.ticker_symbol)')
-def cri(response):
-    cri= []
-    for row in response:
-        if row[1] == 'ISEC SECURITIZADORA S.A' or row[1] == 'VIRGO II COMPANHIA DE SECURITIZACAO' or row[1] == 'VIRGO COMPANHIA DE SECURITIZACAO':
-            if row[4] == datetime.date.today():
-                if row not in cri:
-                    cri.append(row)
-    return cri
-cri = cri(response1)
+        if row[4] == datetime.date.today():
+            if row not in crai:
+                crai.append(row)
+    return crai
+cra = crai(response)
+cri = crai(response1)
 b3 = cri + cra
 
 def b3_sep(b3):
@@ -42,26 +26,19 @@ def b3_sep(b3):
     v_juros = []
     v_amort = []
     v_amex = []
-
     for dic in b3:
         id = dic[0]
-        preco = str(dic[3])
+        preco = str(round(dic[3],3))
+        dicionario["id"] = id
+        dicionario["tipo"] = dic[2]
+        dicionario["pu"] = preco
         if dic[2] == 'PAGAMENTO DE JUROS':
-            dicionario["id"] = id
-            dicionario["tipo"] = 'PAGAMENTO DE JUROS'
-            dicionario["pu"] = preco
             v_juros.append(dicionario)
             dicionario = {}
-        elif dic[2] == 'AMORTIZACAO':
-            dicionario["id"] = id
-            dicionario["tipo"] = 'AMORTIZACAO'
-            dicionario["pu"] = preco
+        elif dic[2] == "AMORTIZACAO":
             v_amort.append(dicionario)
             dicionario = {}
-        elif dic[2] == 'AMORTIZACAO EXTRAORDINARIA':
-            dicionario["id"] = id
-            dicionario["tipo"] = 'AMORTIZACAO EXTRAORDINARIA'
-            dicionario["pu"] = preco
+        elif dic[2] == "AMORTIZACAO EXTRAORDINARIA":
             v_amex.append(dicionario)
             dicionario = {}
     return v_juros, v_amort, v_amex
@@ -90,46 +67,49 @@ filtro = filter(resultg, v_b3)
 def juros(resultg, resultb):
     lista = []
     lista.append(resultg[0]["id"])
-    valorg = resultg[0]['juros']
-    if valorg == None:
+    if resultg[0]['juros'] == None:
         lista.append("Valor não adicionado")
     else:
-        lista.append(round(valorg,3))
+        valorg = str(round(resultg[0]['juros'], 3))
+        lista.append(valorg)
     lista.append(resultb[0]["pu"])
     return lista
 
 juros = juros(filtro, v_juros)
 
-def amort(resultg, resultb):
+def amorta(resultg, resultb):
     lista = []
     lista.append(resultg[0]["id"])
-    if resultb != []:
-        valorg = str(resultg[0]['amortizacao'])
-        valorb = str(resultb[0]["pu"])
+    if resultg[0]["amortizacao"] != None:
+        valorg = str(round(resultg[0]["amortizacao"],3))
     else:
         valorg = "Valor não adicionado"
+    if resultb != []:
+        valorb = str(resultb[0]["pu"])
+    else:
         valorb = "Valor não adicionado"
     lista.append(valorg)
     lista.append(valorb)
     return lista
+amort = amorta(filtro, v_amort)
 
-amort = amort(filtro, v_amort)
 def amex(resultg, resultb):
     lista = []
     lista.append(resultg[0]["id"])
-    if resultb != []:
-        valorg = round(resultg[0]['amex'])
-        valorb = resultb[0]["pu"]
+    if resultg[0]["amex"] != None:
+        valorg = str(round(resultg[0]["amex"],3))
     else:
         valorg = "Valor não adicionado"
+    if resultb != []:
+        valorb = str(resultb[0]["pu"])
+    else:
         valorb = "Valor não adicionado"
     lista.append(valorg)
     lista.append(valorb)
     return lista
 amex = amex(filtro, v_amex)
 
-
-headings=['ID', 'VALOR GALÁXIA', 'VALOR B3']
+headings = ['ID', 'VALOR GALÁXIA', 'VALOR B3']
 
 def valuesf(g):
     listona = []
@@ -137,13 +117,17 @@ def valuesf(g):
     for i in g:
         lista.append(i['titulo'])
         lista.append(i['id'])
-        if i['juros'] == None and i['amortizacao']== None and i['amex'] == None:
-            lista.append('Valor não aplicado no Galáxia')
-            lista.append('Valor não aplicado no Galáxia')
+        if i['juros'] == None:
             lista.append('Valor não aplicado no Galáxia')
         else:
             lista.append(str(i['juros']))
-            lista.append(str(i['amortizacao']))
+        if i['amortizacao']== None:
+            lista.append('Valor não aplicado no Galáxia')
+        else:
+            lista.append(str(i['juros']))
+        if i['amex'] == None:
+            lista.append('Valor não aplicado no Galáxia')
+        else:
             lista.append(str(i['amex']))
         lista.append(i['responsavel'])
         listona.append(lista)
@@ -155,13 +139,12 @@ def valuesb(b3):
     return lista
 listag = valuesf(filtro)
 listab = valuesb(b3)
-
 def comp(filtro, v_b3, string):
     listona = []
     lista = []
     lista.append(v_b3[0]['id'])
-    lista.append(v_b3[0]['pu'])
-    lista.append(filtro[0][string])
+    lista.append(str(v_b3[0]['pu']))
+    lista.append(str(filtro[0][string]))
     listona.append(lista)
     return listona
 compj = comp(filtro, v_juros, 'juros')
@@ -180,25 +163,23 @@ def mensagem( compj, compm, compa):
     m1 = ''
     m2= ''
     m3 = ''
-    m4 = ''
     if compj[0][1] != compj[0][2]:
-        mensagem = ("Olá, {}!\n A PU do título de ID igual a {} se encontra divergente com a B3.".format(filtro[0]['responsavel'], compj[0]))
-        m1= 'JUROS DIVERGENTE'
+        mensagem = ("Olá, {}!\n A PU de ID igual a {} se encontra divergente com a B3.".format(filtro[0]['responsavel'], filtro[0]["id"]))
+        m1= 'JUROS DIVERGENTE/FALTANTE'
     if compm[0][1] != compm[0][2]:
-        mensagem = ("Olá, {}!\n A PU do título de ID igual a {} se encontra divergente com a B3.".format(filtro[0]['responsavel'],compj[0]))
-        m2 = 'AMORTIZAÇÃO DIVERGENTE'
+        mensagem = ("Olá, {}!\n A PU de ID igual a {} se encontra divergente com a B3.".format(filtro[0]['responsavel'],filtro[0]["id"]))
+        m2 = 'AMORTIZAÇÃO DIVERGENTE/FALTANTE'
     if compa[0][1] != compa[0][2]:
-        mensagem = ("Olá, {}!\n A PU do título de ID igual a {} se encontra divergente com a B3.".format(filtro[0]['responsavel'],compj[0]))
-        m3 = "AMEX DIVERGENTE"
-    if filtro[0]['juros'] == None:
-        mensagem = ("Olá, {}!\n A PU do título de ID igual a {} não foi aplicada no galáxia, impossibilitando a validação dos valores da B3.Aguardo o lançamento, Obrigada!".format(filtro[0]['responsavel'],compj[0][0]))
-        m4 = 'DADOS FALTANTES'
-    if compj[0][1] == None:
-        mensagem = ("Olá, {}!\n A PU do título de ID igual a {} não foi aplicada no galáxia, impossibilitando a validação dos valores da B3.Aguardo o lançamento, Obrigada!".format(filtro[0]['responsavel'], compj[0][0]))
-        m4 = 'DADOS FALTANTES'
-    return mensagem, m1, m2, m3, m4
+        mensagem = ("Olá, {}!\n A PU de ID igual a {} se encontra divergente com a B3.".format(filtro[0]['responsavel'],filtro[0]["id"]))
+        m3 = "AMEX DIVERGENTE/FALTANTE"
+    if filtro[0]['juros'] == "None":
+        mensagem = ("Olá, {}!\n A PU de ID igual a {} não foi aplicada no galáxia, impossibilitando a validação dos valores da B3.Aguardo o lançamento, Obrigada!".format(filtro[0]['responsavel'],filtro[0]["id"]))
+    if compj[0][1] == "None":
+        mensagem = ("Olá, {}!\n A PU de ID igual a {} não foi aplicada na B3, impossibilitando a validação dos valores da B3.Aguardo o lançamento, Obrigada!".format(filtro[0]['responsavel'], filtro[0]["id"]))
+    return mensagem, m1, m2, m3
 
-mensagem, m1, m2, m3, m4 = mensagem(compj,compm, compa)
+mensagem, m1, m2, m3= mensagem(compj,compm, compa)
+
 gui.theme('DarkPurple')
 gui.SetOptions(text_color="#e4e4e4", font='Any 11')
 instructions_layout = [
@@ -220,7 +201,7 @@ form_layout = [
                justification='left',
                num_rows=10,
                key='-TABLE-',
-               col_widths=[11, 16, 20, 10, 12],
+               col_widths=[11, 25, 20, 11, 12],
                row_height=35,
                tooltip='B3')]
 ]
@@ -233,7 +214,8 @@ table_layout = [[gui.Text('Juros')],
                num_rows=10,
                key='-TABLE-',
                row_height=35,
-               tooltip='GALÁXIA')]
+               tooltip='GALÁXIA')],
+                [gui.Text(m1, font=('Any', '12', 'bold'), text_color='red')]
 ]
 table2_layout = [[gui.Text('Amortização')],
     [gui.Table(values=compm, headings=headings, max_col_width=38,
@@ -243,7 +225,8 @@ table2_layout = [[gui.Text('Amortização')],
                num_rows=10,
                key='-TABLE-',
                row_height=35,
-               tooltip='GALÁXIA')]
+               tooltip='GALÁXIA')],
+                [gui.Text(m2, font=('Any', '12', 'bold'), text_color='red')]
 ]
 table3_layout = [[gui.Text('Amex')],
     [gui.Table(values=compa, headings=headings, max_col_width=38,
@@ -253,23 +236,23 @@ table3_layout = [[gui.Text('Amex')],
                num_rows=10,
                key='-TABLE-',
                row_height=35,
-               tooltip='GALÁXIA')]
-]
-t = [[gui.Column(table_layout, element_justification='c' ), gui.Column(table2_layout, element_justification='c')
-          , gui.Column(table3_layout, element_justification='c')]]
+               tooltip='GALÁXIA')],
+                [gui.Text(m3, font=('Any', '12', 'bold'), text_color='red')]]
+
+t = [[gui.Column(table_layout, element_justification='c' ), gui.Column(table2_layout, element_justification='c'), gui.Column(table3_layout, element_justification='c')]]
 
 gui.SetOptions(text_color="#e4e4e4", font='Any 11')
 tab_group = [[gui.Push(), gui.Image(filename='img.png'), gui.Push(), gui.Text('Extração de Relatório de Liquidação Diária',size=(40, 1), font=('Any 18')),gui.Push()],
 
     [gui.TabGroup(
         [[gui.Tab('Relatório B3', form_layout, title_color='Purple', background_color='Pink',
-                 tooltip='B3', element_justification='c'),gui.Text(m1),
+                 tooltip='B3', element_justification='c'),
 
           gui.Tab('Relatório Galáxia', instructions_layout, title_color='Purple', background_color='Pink',
-                tooltip='B3', element_justification='c'),gui.Text(m2),
+                tooltip='B3', element_justification='c'),
 
           gui.Tab('Comparação', t, title_color='Blue', background_color='Pink',
-          tooltip='Comparar', element_justification='c'),gui.Text(m3)]],
+          tooltip='Comparar', element_justification='c')]],
         tab_location='centertop',
         title_color='Pink', tab_background_color='Purple', selected_title_color='Purple',
         selected_background_color='Pink', border_width=5), gui.Button('Fechar')
@@ -290,15 +273,8 @@ email = filtro[0]['email']
 if mensagem != None:
     outlook = win32.Dispatch('outlook.application')
     email = outlook.CreateItem(0)
-    email.To = 'fernanda.pereira@virgo.inc'
+    email.To = email
     email.Subject = 'Validação de PUs imputadas na B3'
     email.HTMLBody = mensagem
     email.Send()
-else:
-    outlook = win32.Dispatch('outlook.application')
-    email = outlook.CreateItem(0)
-    email.To = 'fernanda.pereira@virgo.inc'
-    email.Subject = 'PU divergente'
-    email.HTMLBody = "A PU do título que possui ID igual a {} se encontra divergente com a B3."
-    email.Send()
-    print('e-mail enviado com sucesso!')
+    print('email enviado')
